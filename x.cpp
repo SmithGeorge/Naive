75367bc2120b2c2b5833b7ec52e33e6b25e16323
Smith George <htydog@outlook.com>
2020-12-14 01:58:03 +0800

    Update x.cpp

--- x.cpp
+++ x.cpp
@@ -2,15 +2,15 @@
 #include<iostream>
 int main(){
 	std::string user_input = "";
-	int opt = 0;
-	double first_number = 0;
-	double second_number = 0;
+	float opt = 0;
+	float first_number = 0;
+	float second_number = 0;
 	float parse(float arg0,float arg1,float arg2);
 	while(1 == 1){
 		std::cerr << "> ";
 		getline(std::cin,user_input,',');
 		opt = stof(user_input);
-		if (opt < 0 || opt > 3) std::cerr << "Don't use operands except for 0,1,2,3" break;	
+		if (opt < 0 || opt > 3) std::cerr << "Don't use operators except for 0,1,2,3" continue;	
 		getline(std::cin,user_input,',');
 		first_number = stof(user_input);
 		getline(std::cin,user_input,'\n');
